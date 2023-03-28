d5658f420f3b1fb08cfaacd319bf5f53d1727b09
ATATC <futerry@outlook.com>
2023-03-28 17:41:08 -0400

    Switched to Log4j.

--- src/main/java/com/atatctech/heditor/Heditor.java
+++ src/main/java/com/atatctech/heditor/Heditor.java
@@ -5,9 +5,10 @@ import com.atatctech.heditor.pattern.Type;
 import com.atatctech.hephaestus.component.*;
 import com.atatctech.hephaestus.export.fs.ComponentFolder;
 import com.atatctech.packages.basics.Basics;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Objects;
 
 public class Heditor {
@@ -54,44 +55,50 @@ public class Heditor {
 
     public static final PatternExtractor PYTHON = null;
 
-    public static void main(String[] args) throws IOException {
-        int lengthOfArgs = args.length;
-        if (lengthOfArgs < 2) throw new IllegalArgumentException("Must have at least 3 arguments.");
-        File file = new File(args[lengthOfArgs - 1]);
-        String language = args[1].toLowerCase();
-        PatternExtractor patternExtractor;
-        Type type = Type.MARKDOWN;
-        String outputDir = null;
-        File wrapperFile = null;
-        switch (language) {
-            case "java" -> patternExtractor = JAVA;
-            case "python" -> patternExtractor = PYTHON;
-            default -> throw new RuntimeException("Unexpected language: \"" + language + "\".");
-        }
-        for (int i = 2; i < lengthOfArgs; i++) {
-            String arg = args[i];
-            if (arg.startsWith("--type=")) {
-                String typeString = arg.substring(7);
-                type = switch (typeString) {
-                    case "md" -> Type.MARKDOWN;
-                    case "html" -> Type.HTML;
-                    case "p" -> Type.PLAIN_TEXT;
-                    default -> throw new RuntimeException("Unknown type: \"" + typeString + "\".");
-                };
-            } else if (arg.startsWith("--wrapperfile=")) {
-                wrapperFile = new File(arg.substring(14));
-            } else {
-                outputDir = arg;
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger();
+        try {
+            int lengthOfArgs = args.length;
+            if (lengthOfArgs < 2) throw new IllegalArgumentException("Must have at least 3 arguments.");
+            File file = new File(args[lengthOfArgs - 1]);
+            String language = args[1].toLowerCase();
+            PatternExtractor patternExtractor;
+            Type type = Type.MARKDOWN;
+            String outputDir = null;
+            File wrapperFile = null;
+            switch (language) {
+                case "java" -> patternExtractor = JAVA;
+                case "python" -> patternExtractor = PYTHON;
+                default -> throw new IllegalArgumentException("Unexpected language: \"" + language + "\".");
             }
-        }
-        switch (args[0].toLowerCase()) {
-            case "extract" -> {
-                Skeleton skeleton = Utils.extract(file, patternExtractor, type);
-                if (outputDir == null) System.out.println(skeleton);
-                else if (outputDir.endsWith(".expr")) Basics.NativeHandler.writeFile(outputDir, skeleton.expr());
-                else (wrapperFile == null ? new ComponentFolder(skeleton) : new ComponentFolder(skeleton, wrapperFile)).write("C:\\Users\\futer\\Downloads\\Hephaestus");
+            for (int i = 2; i < lengthOfArgs; i++) {
+                String arg = args[i];
+                if (arg.startsWith("--type=")) {
+                    String typeString = arg.substring(7);
+                    type = switch (typeString) {
+                        case "md" -> Type.MARKDOWN;
+                        case "html" -> Type.HTML;
+                        case "p" -> Type.PLAIN_TEXT;
+                        default -> throw new IllegalArgumentException("Unknown type: \"" + typeString + "\".");
+                    };
+                } else if (arg.startsWith("--wrapperfile=")) {
+                    wrapperFile = new File(arg.substring(14));
+                } else {
+                    outputDir = arg;
+                }
             }
-            case "inject" -> throw new UnsupportedOperationException("Injection not supported yet.");
+            switch (args[0].toLowerCase()) {
+                case "extract" -> {
+                    Skeleton skeleton = Utils.extract(file, patternExtractor, type);
+                    if (outputDir == null) logger.info(skeleton);
+                    else if (outputDir.endsWith(".expr")) Basics.NativeHandler.writeFile(outputDir, skeleton.expr());
+                    else
+                        (wrapperFile == null ? new ComponentFolder(skeleton) : new ComponentFolder(skeleton, wrapperFile)).write("C:\\Users\\futer\\Downloads\\Hephaestus");
+                }
+                case "inject" -> throw new UnsupportedOperationException("Injection not supported yet.");
+            }
+        } catch (Exception e) {
+            logger.error(e);
         }
     }
 }
